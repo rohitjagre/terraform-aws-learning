26dc6dbcc422ef856d2fcf69df329fc17420f378
Rohit Jagre <rohitjagre1@gmail.com>
2020-03-24 20:49:50 +0530

    added .gitignore and version

--- intro-to-terraform/main.tf
+++ intro-to-terraform/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "ap-south-1"
+  region  = "ap-south-1"
+  version = 2.54
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 }
