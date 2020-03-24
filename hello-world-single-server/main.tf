22169578f24032d552bf9f3a9f372e0b22798b1f
Rohit Jagre <rohitjagre1@gmail.com>
2020-03-24 21:43:52 +0530

    added security-group for opening ports

--- hello-world-single-server/main.tf
+++ hello-world-single-server/main.tf
@@ -4,8 +4,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0620d12a9cf777c87"
-  instance_type = "t2.micro"
+  ami                    = "ami-0620d12a9cf777c87"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   user_data = <<-EOF
                     #!/bin/bash
@@ -15,5 +16,16 @@ resource "aws_instance" "example" {
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "example_sg" {
+  name = "terraform-example-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
